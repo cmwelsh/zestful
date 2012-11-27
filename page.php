8bea02e8f407c010cdfdc8a79a690dc5d3bffcde
cmwelsh <chris@cmwelsh.com>
2012-11-27 11:07:26 -0700

    code style change to the loop
    I think WordPress users will like this formatting better.

--- page.php
+++ page.php
@@ -13,12 +13,10 @@ get_header(); ?>
 <div id="primary" class="site-content">
     <div id="content" role="main">
 
-        <?php
-        foreach (Zesty::query() as $p) :
-            get_template_part('content', 'page');
-            comments_template('', true);
-        endforeach;
-        ?>
+        <?php foreach (Zesty::query() as $post) : ?>
+            <?php get_template_part('content', 'page'); ?>
+            <?php comments_template('', true); ?>
+        <?php endforeach; ?>
 
     </div><!-- #content -->
 </div><!-- #primary -->
